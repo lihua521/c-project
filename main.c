9e3b93bf145ed58aebaf306e2a5dab21d3d89350
李财宝 <1825306263@qq.com>
2019-01-17 08:52:03 +0800

    添加敏感字符

--- main.c
+++ main.c
@@ -1,34 +1,65 @@
 #include <stdio.h>
-
-int main(){
+char arr[1000];
+int index = 0;
+int isMingan(char x)
+{
+    int count = 0;
+    for (int i = 0; i < index; i++)
+    {
+        if (x == arr[i])
+        {
+            count++;
+        }
+    }
+    if (count == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+int main()
+{
 
     printf("mingancixitong\n");
 
-    while(1){
+    while (1)
+    {
         printf("1---tianjia\n");
         printf("2---shanchu\n");
         printf("3---xianshi\n");
         printf("4---tihuan\n");
         printf("5---tuichu\n");
         int code;
-        scanf("%d",&code);
-        
-        if(code == 1){
+        scanf("%d", &code);
 
+        if (code == 1)
+        {
+            printf("tianjia\n");
+            char m;
+            scanf("%c", &m);
+            scanf("%c", &m);
+            arr[index] = m;
+            index++;
+            printf("dianjihuichejixu\n");
+            char x;
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
-        if(code == 2){
-
+        if (code == 2)
+        {
         }
-        if(code == 3){
-            
+        if (code == 3)
+        {
         }
-        if(code == 4){
-            
+        if (code == 4)
+        {
         }
-        if(code == 5){
-            
+        if (code == 5)
+        {
         }
     }
     return 0;
-
 }
